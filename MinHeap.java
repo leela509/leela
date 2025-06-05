bdbe68aa187dacf7d9cac7df5c351a7726dfc6f4
leela509 <bogathiprasanna@gmail.com>
2025-06-05 15:42:52 +0530

    Add files via upload

---
+++ MinHeap.java
@@ -0,0 +1,79 @@
+public class MinHeap {
+    private int[] heap;
+    private int size;
+    private int capacity;
+
+    public MinHeap(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.heap = new int[capacity];
+    }
+
+    private int parent(int i) {
+        return (i - 1) / 2;
+    }
+
+    private int leftChild(int i) {
+        return 2 * i + 1;
+    }
+
+    private int rightChild(int i) {
+        return 2 * i + 2;
+    }
+
+    private void swap(int i, int j) {
+        int temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+    public void insert(int value) {
+        if (size == capacity) return;
+        heap[size] = value;
+        int current = size;
+        size++;
+        while (current > 0 && heap[current] < heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    public int extractMin() {
+        if (size == 0) return -1;
+        int min = heap[0];
+        heap[0] = heap[size - 1];
+        size--;
+        heapify(0);
+        return min;
+    }
+
+    private void heapify(int i) {
+        int left = leftChild(i);
+        int right = rightChild(i);
+        int smallest = i;
+
+        if (left < size && heap[left] < heap[smallest]) smallest = left;
+        if (right < size && heap[right] < heap[smallest]) smallest = right;
+
+        if (smallest != i) {
+            swap(i, smallest);
+            heapify(smallest);
+        }
+    }
+
+    public int getMin() {
+        if (size == 0) return -1;
+        return heap[0];
+    }
+
+    public static void main(String[] args) {
+        MinHeap minHeap = new MinHeap(10);
+        minHeap.insert(5);
+        minHeap.insert(3);
+        minHeap.insert(8);
+        minHeap.insert(1);
+
+        System.out.println(minHeap.extractMin());
+        System.out.println(minHeap.getMin());
+    }
+}
