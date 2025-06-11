6cb449fb48521cac278548c3294689225baaa17e
leela509 <bogathiprasanna@gmail.com>
2025-06-11 16:13:50 +0530

    Add files via upload

---
+++ Sorting.java
@@ -0,0 +1,11 @@
+ import java.util.Arrays;
+ public class Main{
+     public static void main(String[]args){
+         int[] arr={5,2,3,1};
+System.out.println("Original Array");
+System.out.println(Arrays.toString(arr));
+Arrays.sort(arr);
+System.out.println("Sorted Array:");
+System.out.println(Arrays.toString(arr));
+  }
+}
